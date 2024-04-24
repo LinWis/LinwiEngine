90fd1ad2195ae8ac65356830c89dad27890efe39
viktor turbin <turbinvr1@gmail.com>
2024-04-24 13:31:28 +0300

    add window layer for Windows

--- testbed/src/main.c
+++ testbed/src/main.c
@@ -1,12 +1,24 @@
 #include <core/logger.h>
 #include <core/asserts.h>
 
+//TODO: test platform
+#include <platform/platform.h>
+
 int main(void) {
     LFATAL("A test message: %f", 3.14f);
     LERROR("A test message: %f", 3.14f);
     LWARN("A test message: %f", 3.14f);
 
-    LASSERT_MSG(1 == 0, "test");
+    platform_state state;
+    if (platform_startup(&state, "Linwi Engine Testbed", 100, 100, 1280, 720)) {
+        while (TRUE) {
+            if (!platform_pump_messages(&state)) {
+                break;
+            }
+        }
+        platform_shutdown(&state);
+    }
+
 
     return 0;
 }
